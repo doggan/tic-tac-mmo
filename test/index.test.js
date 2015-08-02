e8c4652fd174f9f0f761356b08dfe0a377a68e91
Shyam Guthikonda <shyamguth@gmail.com>
2015-08-02 14:08:55 -0700

    Refactoring.

--- test/index.test.js
+++ test/index.test.js
@@ -1,26 +1,26 @@
 'use strict';
 
 var expect = require('chai').expect,
-    util = require('./../lib/util'),
-    AiPlayer = require('./../lib/ai_player');
+    game_util = require('./../lib/game_util'),
+    Ai = require('./../lib/ai');
 
 var COL_COUNT = 3;
 var ROW_COUNT = 3;
 
-var Marker = util.Marker;
+var Marker = game_util.Marker;
 
 function createBoardInfo() {
     return {
         colCount: COL_COUNT,
         rowCount: ROW_COUNT,
-        cells: util.createBoard(COL_COUNT, ROW_COUNT)
+        cells: game_util.createBoard(COL_COUNT, ROW_COUNT)
     };
 }
 
 describe('board utilities', function() {
     it('should properly generate move list (empty board)', function(done) {
         var boardInfo = createBoardInfo();
-        var moveList = AiPlayer.generateMoves(boardInfo);
+        var moveList = Ai.generateMoves(boardInfo);
         expect(moveList.length).to.equal(COL_COUNT * ROW_COUNT);
         done();
     });
@@ -28,7 +28,7 @@ describe('board utilities', function() {
     it('should properly generate move list (partially filled board)', function(done) {
         var boardInfo = createBoardInfo();
         boardInfo.cells[1][1] = Marker.O;
-        var moveList = AiPlayer.generateMoves(boardInfo);
+        var moveList = Ai.generateMoves(boardInfo);
         expect(moveList.length).to.equal(COL_COUNT * ROW_COUNT - 1);
         done();
     });
@@ -40,7 +40,7 @@ describe('board utilities', function() {
                 boardInfo.cells[x][y] = Marker.O;
             }
         }
-        var moveList = AiPlayer.generateMoves(boardInfo);
+        var moveList = Ai.generateMoves(boardInfo);
         expect(moveList.length).to.equal(0);
         done();
     });
@@ -52,7 +52,7 @@ describe('board utilities', function() {
         boardInfo.cells[1][0] = Marker.O;
         boardInfo.cells[2][0] = Marker.O;
 
-        var moveList = AiPlayer.generateMoves(boardInfo);
+        var moveList = Ai.generateMoves(boardInfo);
         expect(moveList.length).to.equal(0);
         done();
     });
@@ -64,7 +64,7 @@ describe('board utilities', function() {
         boardInfo.cells[1][0] = Marker.X;
         boardInfo.cells[2][0] = Marker.X;
 
-        var moveList = AiPlayer.generateMoves(boardInfo);
+        var moveList = Ai.generateMoves(boardInfo);
         expect(moveList.length).to.equal(0);
         done();
     });
@@ -74,7 +74,7 @@ describe('minimax evaluation function', function() {
     it('should properly calculate a score (empty board)', function(done) {
         var boardInfo = createBoardInfo();
 
-        expect(AiPlayer.evaluateScore(boardInfo, Marker.O)).to.equal(0);
+        expect(Ai.evaluateScore(boardInfo, Marker.O)).to.equal(0);
         done();
     });
 
@@ -85,7 +85,7 @@ describe('minimax evaluation function', function() {
         boardInfo.cells[1][0] = Marker.O;
         boardInfo.cells[2][0] = Marker.O;
 
-        expect(AiPlayer.evaluateScore(boardInfo, Marker.O)).to.equal(1);
+        expect(Ai.evaluateScore(boardInfo, Marker.O)).to.equal(1);
         done();
     });
 
@@ -96,7 +96,7 @@ describe('minimax evaluation function', function() {
         boardInfo.cells[1][0] = Marker.X;
         boardInfo.cells[2][0] = Marker.X;
 
-        expect(AiPlayer.evaluateScore(boardInfo, Marker.O)).to.equal(-1);
+        expect(Ai.evaluateScore(boardInfo, Marker.O)).to.equal(-1);
         done();
     });
 });
@@ -104,15 +104,38 @@ describe('minimax evaluation function', function() {
 describe('algorithm performance', function() {
     var ITERATION_COUNT = 10;
 
+    it('should benchmark performance for random moves', function(done) {
+        this.timeout(0);
+
+        var boardInfo = createBoardInfo();
+
+        Ai.setStrategy(Ai.STRATEGIES.RandomOpenCell);
+
+        var totalTime = 0.0;
+        for (var i = 0; i < ITERATION_COUNT; i++) {
+            var hrstart = process.hrtime();
+            Ai.getMove(boardInfo, Marker.X);
+            var hrend = process.hrtime(hrstart)[1] / 1000000;
+            totalTime += hrend;
+        }
+
+        var avgTime = totalTime / ITERATION_COUNT;
+        console.log('Elapsed time (random): ' + avgTime + ' (ms)');
+
+        done();
+    });
+
     it('should benchmark minimax performance for generating the entire game tree', function(done) {
         this.timeout(0);
 
         var boardInfo = createBoardInfo();
 
+        Ai.setStrategy(Ai.STRATEGIES.Minimax);
+
         var totalTime = 0.0;
         for (var i = 0; i < ITERATION_COUNT; i++) {
             var hrstart = process.hrtime();
-            AiPlayer.doMinimax(boardInfo, Marker.X);
+            Ai.getMove(boardInfo, Marker.X);
             var hrend = process.hrtime(hrstart)[1] / 1000000;
             totalTime += hrend;
         }
@@ -128,10 +151,12 @@ describe('algorithm performance', function() {
 
         var boardInfo = createBoardInfo();
 
+        Ai.setStrategy(Ai.STRATEGIES.AlphaBetaPruning);
+
         var totalTime = 0.0;
         for (var i = 0; i < ITERATION_COUNT; i++) {
             var hrstart = process.hrtime();
-            AiPlayer.doAlphaBetaPruning(boardInfo, Marker.X);
+            Ai.getMove(boardInfo, Marker.X);
             var hrend = process.hrtime(hrstart)[1] / 1000000;
             totalTime += hrend;
         }
